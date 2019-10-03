836d2a06d596535905292aa69b0891290064d269
ekoremdev@gmail.com <ekoremdev@gmail.com>
2019-10-03 17:27:03 +0400

    project created, and module created

---
+++ library01/src/main/java/com/example/library01/Library01Main.java
@@ -0,0 +1,13 @@
+package com.example.library01;
+
+import android.util.Log;
+
+public class Library01Main {
+
+    private static final String TAG = "Library01Main";
+
+    public static void d(String message){
+        Log.d(TAG, message);
+    }
+
+}
